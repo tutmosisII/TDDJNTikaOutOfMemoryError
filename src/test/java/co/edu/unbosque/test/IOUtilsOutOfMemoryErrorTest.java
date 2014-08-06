520cb9944d603d06dc104144701cf9ae9364be08
aleon <aleon@easysol.net>
2014-08-06 16:44:21 -0500

    Posible corrección del error

--- src/test/java/co/edu/unbosque/test/IOUtilsOutOfMemoryErrorTest.java
+++ src/test/java/co/edu/unbosque/test/IOUtilsOutOfMemoryErrorTest.java
@@ -3,6 +3,7 @@ package co.edu.unbosque.test;
 import java.io.IOException;
 import java.io.InputStream;
 import org.apache.tika.io.IOUtils;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 /**
@@ -19,5 +20,17 @@ public class IOUtilsOutOfMemoryErrorTest {
             }
         };
         byte x[]=IOUtils.toByteArray(in);
+    }
+    
+    @Test
+    public void probandoLimiteAUsar() throws IOException{
+        InputStream in=new InputStream() {
+            @Override
+            public int read() throws IOException {
+                return 1;
+            }
+        };
+        byte x[]=co.edu.unbosque.io.IOUtils.toByteArray(in,12288);
+        Assert.assertTrue(12288<=x.length);
     }
 }
